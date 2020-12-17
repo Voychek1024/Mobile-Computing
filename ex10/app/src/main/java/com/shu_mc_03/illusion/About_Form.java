43a2c105956180e8b0947f2a8133be4e781725cc
李嘉琪 <erusea@shu.edu.cn>
2020-12-17 16:44:18 +0800

    project fix

--- ex10/app/src/main/java/com/shu_mc_03/illusion/About_Form.java
+++ ex10/app/src/main/java/com/shu_mc_03/illusion/About_Form.java
@@ -1,8 +1,18 @@
 package com.shu_mc_03.illusion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class About_Form extends AppCompatActivity {
 
@@ -10,5 +20,20 @@ public class About_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView textView = findViewById(R.id.text_link);
+        String text = "GITHUB项目地址";
+        SpannableString ss = new SpannableString(text);
+
+        ClickableSpan span = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Voychek1024/Mobile-Computing"));
+                startActivity(browserIntent);
+            }
+        };
+        ss.setSpan(span, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
